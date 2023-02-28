f2a4d177cacaf578ce73d6ffab4310570966f451
Carlos lima <carlos.lima@siedos.com.br>
2023-02-28 05:08:50 -0300

    Change image in document with extension docx

---
+++ app/services/serenity/images_processor.rb
@@ -0,0 +1,36 @@
+module Serenity
+  class ImagesProcessor
+    include Debug
+
+    IMAGE_DIR_NAME = "Pictures"
+
+    # Example: { "rId1"=>"roo_media_image1.png",
+    #            "rId2"=>"roo_media_image2.png",
+    #            "rId3"=>"roo_media_image3.png" }
+
+    def initialize(xml_content, context)
+      @replacements = []
+      @images = eval('@images', context)
+      @xml_content = xml_content
+    end
+
+    def generate_replacements
+      require 'nokogiri'
+
+      if @images && @images.kind_of?(Hash)
+        xml_data = Nokogiri::XML(@xml_content)
+
+        @images.each do |image_name, replacement_path|
+          if node = xml_data.xpath("//draw:frame[@draw:name='Figura1']/draw:image").first
+            placeholder_path = node.attribute('href').value
+            odt_image_path = ::File.join(IMAGE_DIR_NAME, ::File.basename(placeholder_path))
+
+            @replacements << [odt_image_path, replacement_path]
+          end
+        end
+      end
+
+      @replacements
+    end
+  end
+end
--- app/services/serenity/template.rb
+++ app/services/serenity/template.rb
@@ -1,5 +1,6 @@
 require 'zip/zip'
 require 'fileutils'
+require 'docx'
 
 module Serenity
   class Template
@@ -15,16 +16,42 @@ module Serenity
       tmpfiles = []
       Zip::ZipFile.open(@template_output) do |zipfile|
 
+        arquivos_imgs = %w(word/_rels/document.xml.rels [Content_Types].xml)
+
+
         if is_xlsx_file?(@template)
           Serenity::XlsxCompiler.new(@template, context, @template_output).evaluate_excel_file
         else
-          if template.include?('docx')
-            arquivos = %w(word/document.xml)
+          if @template.include?('docx')
+            arquivos = %w(word/document.xml word/_rels/document.xml.rels [Content_Types].xml)
+            # <Override PartName="/word/media/image1.png" ContentType="image/png"/>
+            # /word/_rels/document.xml.rels
+            #
           else
             arquivos = %w(content.xml styles.xml)
           end
           arquivos.each do |xml_file|
             content = zipfile.read(xml_file)
+
+
+          if arquivos_imgs.include?(xml_file)
+            content = Serenity::WordImage.new(@template, context, content, @template_output).adicionar_imagem_no_word(1, 2, xml_file, zipfile)
+          end
+          content
+
+
+            #add img tentativa 1
+            unless @template.include?('docx')
+              unless zipfile.find_entry('Pictures/logo.png')
+                zipfile.add('Pictures/logo.png',  ::File.join(Rails.public_path, 'logo.png'))
+              end
+              images_replacements = ImagesProcessor.new(content, context).generate_replacements
+              images_replacements.each do |r|
+                content = content.gsub(r.first, "Pictures/#{r.last}")
+              end
+            end
+
+
             eruby = Erubis::Eruby.new(content.force_encoding('ASCII-8BIT').force_encoding('UTF-8')
                                              .gsub('&lt;%=', '<%=').gsub('&lt;%', '<%').gsub('%&gt;', '%>'), :bufvar=>'@_out')
             out = eval(eruby.src, context)
